39daa21f1d4f5700743bef4b9a2a861131356779
JJ Merelo <jjmerelo@gmail.com>
2019-02-05 14:15:14 +0100

    Adds results to changelog

--- utils/charts-gecco-2019.R
+++ utils/charts-gecco-2019.R
@@ -3,12 +3,15 @@ library(ggplot2)
 library(ggthemes)
 
 data.freqs.threads <- read.csv('../data/gecco-2019-freqs-threads.csv')
-
-
 ggplot(data.freqs.threads,aes(x=Threads,y=Evaluations,group=Threads))+geom_boxplot()+scale_x_log10()+theme_tufte()+labs(x="Threads",y="Evaluations",title="Generation gap = 8")
 ggplot(data.freqs.threads,aes(x=Threads,y=Time,group=Threads))+geom_boxplot()+scale_x_log10()+theme_tufte()+labs(x="Threads",y="Time",title="Generation gap = 8")
 ggplot(data.freqs.threads,aes(x=Threads,y=Evaluations/Time,group=Threads))+geom_boxplot()+scale_x_log10()+theme_tufte()+labs(x="Threads",y="Evaluation/time",title="Generation gap = 8")
 
+
+data.compress.threads <- read.csv('../data/gecco-2019-compress-threads.csv')
+ggplot(data.compress.threads,aes(x=Threads,y=Evaluations,group=Threads))+geom_boxplot()+scale_x_log10()+theme_tufte()+labs(x="Threads",y="Evaluations",title="Generation gap = 8")
+ggplot(data.compress.threads,aes(x=Threads,y=Time,group=Threads))+geom_boxplot()+scale_x_log10()+theme_tufte()+labs(x="Threads",y="Time",title="Generation gap = 8")
+ggplot(data.compress.threads,aes(x=Threads,y=Evaluations/Time,group=Threads))+geom_boxplot()+scale_x_log10()+theme_tufte()+labs(x="Threads",y="Evaluation/time",title="Generation gap = 8")
 
 data.freqs.generations <- read.csv('../data/gecco-2019-freqs-generations.csv')
 
