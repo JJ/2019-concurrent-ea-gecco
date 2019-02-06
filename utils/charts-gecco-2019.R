649ba8466d4bb42f599aa70684bc221fe256d7c1
JJ Merelo <jjmerelo@gmail.com>
2019-02-06 07:54:19 +0100

    And another disappointing result from the thread part

--- utils/charts-gecco-2019.R
+++ utils/charts-gecco-2019.R
@@ -15,7 +15,6 @@ ggplot(data.compress.threads,aes(x=Threads,y=Evaluations/Time,group=Threads))+ge
 
 data.freqs.generations <- read.csv('../data/gecco-2019-freqs-generations.csv')
 
-
 ggplot(data.freqs.generations,aes(x=Generation.Gap,y=Evaluations,group=Generation.Gap))+geom_boxplot()+scale_x_log10()+theme_tufte()+labs(x="Generations",y="Evaluations",title="Generation gap = 8")
 ggplot(data.freqs.generations,aes(x=Generation.Gap,y=Time,group=Generation.Gap))+geom_boxplot()+scale_x_log10()+theme_tufte()+labs(x="Generations",y="Time",title="Generation gap = 8")
 ggplot(data.freqs.generations,aes(x=Generation.Gap,y=Evaluations/Time,group=Generation.Gap))+geom_boxplot()+scale_x_log10()+theme_tufte()+labs(x="Generations",y="Evaluation/time",title="Generation gap = 8")
